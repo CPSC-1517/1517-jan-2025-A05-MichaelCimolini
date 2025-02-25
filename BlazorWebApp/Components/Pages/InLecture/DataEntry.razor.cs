572c79b5f31370689d67849912c1348a1d96dda7
Michael Cimolini <mcimolini@nait.ca>
2025-02-25 14:32:48 -0700

    Submit updated to write to CSV

--- BlazorWebApp/Components/Pages/InLecture/DataEntry.razor.cs
+++ BlazorWebApp/Components/Pages/InLecture/DataEntry.razor.cs
@@ -1,4 +1,5 @@
-﻿using OOPsReview;
+﻿using Microsoft.AspNetCore.Components;
+using OOPsReview;
 
 namespace BlazorWebApp.Components.Pages.InLecture
 {
@@ -13,6 +14,15 @@ namespace BlazorWebApp.Components.Pages.InLecture
         private double EmploymentYears = 0.0;
         private SupervisoryLevel EmploymentLevel = SupervisoryLevel.Entry;
         private DateTime EmploymentStart = DateTime.Today;
+
+        private Employment employmentData = null;
+
+        //injected services
+        //These are treated as auto-implemeted properties.
+
+        //WebHostEnvironment lets us get information like filepath from our application.
+        [Inject]
+        public IWebHostEnvironment webHostEnvironment { get; set; }
 
 
         protected override void OnInitialized()
@@ -48,7 +58,46 @@ namespace BlazorWebApp.Components.Pages.InLecture
 
             if (ErrorMessages.Count == 0)
             {
-                FeedbackMsg = $"Success:{EmploymentTitle} {EmploymentYears} {EmploymentStart} {EmploymentLevel}";
+                //FeedbackMsg = $"Success:{EmploymentTitle} {EmploymentYears} {EmploymentStart} {EmploymentLevel}";
+
+                //Want to do File IO with our collected information
+                //File IO tends to fail, so we use try catch.
+
+                try
+                {
+                    employmentData = new Employment(EmploymentTitle, EmploymentLevel, EmploymentStart, EmploymentYears);
+
+                    //Where do we want to write this to?
+
+                    //Lets find where our application is. This finds our wwwroot folder.
+                    string ApplicationPathName = webHostEnvironment.ContentRootPath;
+
+                    //The @ symbol treats characters as they appear. No need to /terminate characters.
+                    string CSVFilePath = $@"{ApplicationPathName}/Data/Employment.csv";
+
+                    //Better to use a variable when writing files as we'll often be looping or changing it.
+                    string line = $"{employmentData.ToString()}\n";
+
+                    //Wrties our line out to the CSVFilePath file.
+                    System.IO.File.AppendAllText(CSVFilePath, line);
+
+                    FeedbackMsg = "Success!";
+                }
+                //Catches any errors from null or missing data passed to Employment.
+                catch (ArgumentNullException ex)
+                {
+                    ErrorMessages.Add($"Missing Data", GetInnerException(ex).Message);
+                }
+                //Catches any errors from bad data passed to Employment.
+                catch (ArgumentException ex)
+                {
+                    ErrorMessages.Add($"Bad Data", GetInnerException(ex).Message);
+                }
+                //Catches anything else (File IO errors, etc).
+                catch (Exception ex)
+                {
+                    ErrorMessages.Add($"System Error", GetInnerException(ex).Message);
+                }
             }
         }
 
@@ -65,6 +114,17 @@ namespace BlazorWebApp.Components.Pages.InLecture
             EmploymentYears = 0.0;
             EmploymentStart = DateTime.Today;
             EmploymentLevel = SupervisoryLevel.Entry;
+        }
+
+        //Loops through our exception until it finds the root cause.
+        private Exception GetInnerException(Exception ex)
+        {
+            while (ex.InnerException != null)
+            {
+                ex = ex.InnerException;
+            }
+
+            return ex;
         }
     }
 }
