bb7cab9ed928a18050948d2056abfc137e50c0d0
Michael Cimolini <mcimolini@nait.ca>
2025-03-07 11:03:01 -0700

    Set up Extensions class and first Service.

---
+++ CSWebApp/WestWindSystem/BLL/BuildVersionServices.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WestWindSystem.DAL;
+using WestWindSystem.Entities;
+
+namespace WestWindSystem.BLL
+{
+    //Anything in BLL will generally be public as this is how other applications/classes/etc should access
+    //our database.
+    public class BuildVersionServices
+    {
+        //this makes a copy of our context usable only by this class.
+        //readonly means it can only be set in our constructor.
+        private readonly WestWindContext _context;
+
+        internal BuildVersionServices(WestWindContext context)
+        {
+            _context = context;
+        }
+
+        #region Services
+        /// <summary>
+        /// This will return the 1st record from the BuildVersion table or null
+        /// </summary>
+        /// <returns>1st or Default</returns>
+        public BuildVersion? GetBuildVersion() //BuildVersionGet -or- BuildVersion_Get
+        {
+            //Using IEnumerable (base base class of List)
+            //Allows us to iterate through our records, but nothing else
+            //This prevents our DB from runnin an unnecessary query
+            IEnumerable<BuildVersion> records = _context.BuildVersions;
+
+            return records.FirstOrDefault();
+        }
+        #endregion
+    }
+}
---
+++ CSWebApp/WestWindSystem/WestWindExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WestWindSystem.BLL;
+using WestWindSystem.DAL;
+
+namespace WestWindSystem
+{
+    /// <summary>
+    /// Public so that it can be accessed from our other applications.
+    /// 
+    /// This class is going to hold all of our extension methods.
+    /// 
+    /// It will also be where we're adding them to IServiceCollection.
+    /// 
+    /// Any services added to this class can be used anywhere outside of our System.
+    /// </summary>
+    public static class WestWindExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services">The collection to register to.</param>
+        /// <param name="options">Our DB connection string.</param>
+        public static void WestWindExtensionsServices(this IServiceCollection services, Action<DbContextOptionsBuilder> options)
+        {
+            //Adds our Database context to our service list with the connection string.
+            services.AddDbContext<WestWindContext>(options);
+
+            //Add every service we will be using with .AddTransient<T>
+            #region Transient Services
+
+            services.AddTransient<BuildVersionServices>((serviceProvider) =>
+            {
+                var context = serviceProvider.GetService<WestWindContext>();
+
+                return new BuildVersionServices(context);
+            });
+
+            #endregion
+        }
+    }
+}
