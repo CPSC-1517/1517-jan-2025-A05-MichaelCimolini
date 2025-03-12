d9896db665ae710369ccbd9fc8fe2873c44ac144
Michael Cimolini <mcimolini@nait.ca>
2025-03-12 10:23:46 -0600

    Created Shipper and Shipment Services Implemented Shipper Services.

---
+++ CSWebApp/WestWindSystem/BLL/ShipmentServices.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WestWindSystem.DAL;
+using WestWindSystem.Entities;
+
+namespace WestWindSystem.BLL
+{
+    //Anything in BLL will generally be public as this is how other applications/classes/etc should access
+    //our database.
+    public class ShipmentServices
+    {
+        //this makes a copy of our context usable only by this class.
+        //readonly means it can only be set in our constructor.
+        private readonly WestWindContext _context;
+
+        internal ShipmentServices(WestWindContext context)
+        {
+            _context = context;
+        }
+
+        #region Services
+        /// <summary>
+        /// Get any shippments in the given year and month.
+        /// </summary>
+        /// <param name="year">4 digit year yyyy (1950-present)</param>
+        /// <param name="month">1 or 2 digit month mm (1-12)</param>
+        /// <returns></returns>
+        public List<Shipment> GetShipmentsByYearAndMonth(int year, int month) //ShipmentGet -or- Shipment_Get
+        {
+            //Using IEnumerable (base base class of List)
+            //Allows us to iterate through our records, but nothing else
+            //This prevents our DB from runnin an unnecessary query
+            IEnumerable<Shipment> records = _context.Shipments
+                                    .Where(shipment => shipment.ShippedDate.Year == year
+                                                    && shipment.ShippedDate.Month == month)
+                                    .OrderBy(shipment => shipment.ShippedDate);
+
+            //This is the same result as above, but requires 2 DB queries instead of 1.
+            //IEnumerable<Shipment> records = _context.Shipments.Where(shipment => shipment.ShippedDate.Year == year && shipment.ShippedDate.Month == month);
+  
+            return records.ToList();
+        }
+        #endregion
+    }
+}
---
+++ CSWebApp/WestWindSystem/BLL/ShipperServices.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WestWindSystem.DAL;
+using WestWindSystem.Entities;
+
+namespace WestWindSystem.BLL
+{
+    //Anything in BLL will generally be public as this is how other applications/classes/etc should access
+    //our database.
+    public class ShipperServices
+    {
+        //this makes a copy of our context usable only by this class.
+        //readonly means it can only be set in our constructor.
+        private readonly WestWindContext _context;
+
+        internal ShipperServices(WestWindContext context)
+        {
+            _context = context;
+        }
+
+        #region Services
+        /// <summary>
+        /// This will return all Shipper records.
+        /// 
+        /// This could return a huge number of records. Must be careful to only use on small
+        /// datasets.
+        /// 
+        /// </summary>
+        /// <returns>A list of records (may be empty)</returns>
+        public List<Shipper> GetAllShippers() //ShipperGet -or- Shipper_Get
+        {
+            //Using IEnumerable (base base class of List)
+            //Allows us to iterate through our records, but nothing else
+            //This prevents our DB from runnin an unnecessary query
+            IEnumerable<Shipper> records = _context.Shippers;
+
+
+            //This will sort alphabetically A->Z
+            return records.OrderBy(record => record.ShipperDescription).ToList();
+        }
+
+
+        /// <summary>
+        /// Returns the Shipper with matching ID. Null if no Shipper found.
+        /// </summary>
+        /// <param name="id">The target ID.</param>
+        /// <returns>The matching Shipper or null.</returns>
+        public Shipper? GetShipperByID(int id)
+        {
+            Shipper? Shipper = null;
+
+            //FirstOrDefault can take a predicate to further refine our search.
+            Shipper = _context.Shippers.FirstOrDefault(x => x.ShipperID == id);
+
+            return Shipper;
+        }
+        #endregion
+    }
+}
