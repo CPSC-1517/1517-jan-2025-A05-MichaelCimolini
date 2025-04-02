41e37e9f0cf5dced8e39260e260609afb5adb4ac
Michael Cimolini <mcimolini@nait.ca>
2025-04-02 11:21:43 -0600

    Implemented add and update methos

--- CSWebApp/WestWindSystem/BLL/ProductServices.cs
+++ CSWebApp/WestWindSystem/BLL/ProductServices.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -45,10 +46,100 @@ namespace WestWindSystem.BLL
             return record;
         }
 
+        #region CRUD Queries
+
+        /// <summary>
+        /// Assume there is a business rule that items can not have the same Supplier, Product Name, and Quantity per Unit
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
         public int AddProduct(Product product)
         {
-            throw new NotImplementedException();
+            //Validate we have data
+
+            if (product is null)
+            { 
+                throw new ArgumentNullException("Product Information Required!");
+            }
+
+            //Our PKey is an Identity key, so no need to check if it exists
+
+            bool exists = false;
+
+            //Test our business logic
+            exists = _context.Products.Any(
+                        prod => prod.SupplierID == product.SupplierID &&
+                        prod.ProductName.Equals(product.ProductName) &&
+                        prod.QuantityPerUnit.Equals(product.QuantityPerUnit)
+                        );
+
+            if (exists)
+            {
+                //Generally you want to include more info than this
+                throw new ArgumentException($"Product already exists!");
+            }
+
+            //We assume our data is safe at this point
+
+            //Step 1: Data Staging
+            //This data will only be in local memory
+            //We can make any final changes at this point ie: Foreign keys, stock values, data not in our form, etc.
+
+            _context.Products.Add(product);
+
+            //Step 2: Commit
+            //This pushes our data to our database
+            //Any Entity Annotation Validation will happen before the data is sent
+
+            _context.SaveChanges();
+
+            //This will set the the PKey ID of our product
+
+            return product.ProductID;
         }
+
+        public int UpdateProduct(Product product)
+        {
+            if (product is null)
+            {
+                throw new ArgumentNullException("Product Information Required!");
+            }
+
+            //Check if the ID already exists. Can't update a product that doesn't exist.
+
+            if(!_context.Products.Any(prod => prod.ProductID == product.ProductID))
+            {
+                throw new ArgumentException("Product with this ID doesn't exists.");
+            }
+
+            //Check our business logic, could be different logic than create
+            bool exists = false;
+
+            //Test our business logic
+            exists = _context.Products.Any(
+                        prod => prod.SupplierID == product.SupplierID &&
+                        prod.ProductName.Equals(product.ProductName) &&
+                        prod.QuantityPerUnit.Equals(product.QuantityPerUnit) &&
+                        prod.ProductID != product.ProductID //Make sure the product is unique
+                        );
+
+            if (exists)
+            {
+                //Generally you want to include more info than this
+                throw new ArgumentException($"Product already exists!");
+            }
+
+            //This handles checking all of our fields and only modifiying the ones that changed.
+            EntityEntry<Product> updating = _context.Entry(product);
+
+            updating.State = EntityState.Modified;
+
+            //returns the number of records updated
+            return _context.SaveChanges();
+        }
+
+        #endregion
 
         #endregion
 
